8e9a5a6a49351e15055912be1110c9d943c71802
bipinkatuwal567 <bipinkatuwal098@gmail.com>
2024-04-07 20:18:58 +0545

    first commit

---
+++ src/components/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar">
+    <h1><span>Falano</span> Academy</h1>
+    <ul class="navbar-items">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+    <a id="join-us-btn" href="#">
+        Join us
+    </a>
+</nav>
---
+++ src/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./components/navbar.css">
+</head>
+
+<body>
+    <div id="root">
+        <?php
+        require "./components/navbar.php";
+        ?>
+
+        <main class="hero-section">
+            <div class="hero-content">
+                <h2>Welcome to the best <span>Football</span> Academy</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, commodi? Deserunt voluptatem aliquam nobis voluptates?</p>
+                <button><a href="#">Join Now</a></button>
+            </div>
+        </main>
+    </div>
+</body>
+
+</html>
